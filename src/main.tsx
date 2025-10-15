21b340b2255a1f1c44963f00c1a45ca22fb92fa2
MariaBarbosa8 <maria.barbosa8@estudante.ifms.edu.br>
2025-10-15 10:52:16 -0400

    mudei

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter , Routes , Route} from 'react-router-dom'
+import Login from './componentes/login/login.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-    <Route>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/login" element={<p>Login</p>} />
+        <Route path="/login" element={<Login/>} />
       </Routes>
-    </Route>
     </BrowserRouter>
-    <App />
   </StrictMode>,
 )
